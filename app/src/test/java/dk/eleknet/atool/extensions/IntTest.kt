b370326434bbe0f04a11cd35fc6f3697b16b1426
Alexander Murillo <alexandersalemander@gmail.com>
2018-09-19 15:49:10 +0200

    Renames int tests

--- app/src/test/java/dk/eleknet/atool/extensions/IntTest.kt
+++ app/src/test/java/dk/eleknet/atool/extensions/IntTest.kt
@@ -6,14 +6,14 @@ import org.junit.Test
 class IntTest {
 
     @Test
-    fun doubleTest_orValueReturnsObjectValue() {
+    fun intTest_orValueReturnsObjectValue() {
         val value: Int? = 3
         val result = value.orValue(4)
         TestCase.assertEquals("Unexpected int value", 3, result)
     }
 
     @Test
-    fun doubleTest_orValueReturnsSuppliedValue() {
+    fun intTest_orValueReturnsSuppliedValue() {
         val value: Int? = null
         val result = value.orValue(5)
         TestCase.assertEquals("Unexpected int value", 5, result)
